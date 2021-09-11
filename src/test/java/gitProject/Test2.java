62e17365b906665d70fa54e4774a71b7edff97fe
Cemil99 <teostore99@gmail.com>
2021-09-11 22:15:24 +0300

    test2 second commit

---
+++ src/test/java/gitProject/Test2.java
@@ -0,0 +1,8 @@
+package gitProject;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("my second file");
+        System.out.println("more work");
+    }
+}
